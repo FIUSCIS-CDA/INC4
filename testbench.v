4f30eb6a589a59724c8ce72fa2372512b43a81a6
Trevor Cickovski <movingpictures83@gmail.com>
2023-08-15 11:23:06 -0400

    Upgraded quartus, added printout of testcase to testbench

--- testbench.v
+++ testbench.v
@@ -7,6 +7,7 @@ wire[3:0] S;
 INC4 myINC(A[3], A[2], A[1], A[0], S[3], S[2], S[1], S[0]);
 
 initial begin
+$display("Testing A=9");
 A=9;   #10; 
 if (S !== A+1) begin
            $display("Error with A=%d: Should also be %d but got %d", A, A+4, S); $stop;
